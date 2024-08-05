1e0f35839951e29a7394c1262cad115a856b38b3
Musoxon <msx.tuit@gmail.com>
2024-08-05 14:27:15 +0500

    Formatted

--- test/to_arabic_number_test.dart
+++ test/to_arabic_number_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:test/test.dart';
 
 import 'package:to_arabic_number/to_arabic_number.dart';
@@ -9,7 +8,8 @@ void main() {
       expect(Arabic.number('0123456789'), '٠١٢٣٤٥٦٧٨٩');
     });
     test("Test 1", () {
-      expect(Arabic.number('The year is 2024 and the time is 12:45 PM.'), 'The year is ٢٠٢٤ and the time is ١٢:٤٥ PM.');
+      expect(Arabic.number('The year is 2024 and the time is 12:45 PM.'),
+          'The year is ٢٠٢٤ and the time is ١٢:٤٥ PM.');
     });
   });
 }
